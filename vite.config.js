3419cb0db32c92fa0e176cb6b397c5fa2c867f68
skietz01 <skietz01@gmail.com>
2026-06-24 10:05:28 -0400

    Host on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/shd-virtual-construction-partners-website/', 
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
