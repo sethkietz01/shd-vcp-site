2c4287c251d489adcfe0fd813b311457f6223498
skietz01 <skietz01@gmail.com>
2026-06-24 11:57:07 -0400

    Styling, formatting, and add Overview section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import logo from './assets/shd_virtual_construction_logo.jpg';
 import sampleImage from './assets/sample-image.jpg';
 import './App.css'
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <>
       <div className="header">
-        <h1>SHD Virtual Construction Partners</h1>
+        <div className="title">
+          <h1>SHD Virtual Construction Partners</h1>
+        </div>
         <div className="navbar">
           <nav>
             <p>Placeholder</p> 
@@ -22,13 +25,39 @@ function App() {
         </div>
       </div>
 
+      <div className="overview">
+        <h1>Overview</h1>
+        <p className="overview-text">SHD Virtual Construction Partners is a BIM and virtual construction firm focused on delivering fast, 
+          accurate, and constructable digital solutions that support real-world building. 
+          <br />
+          <br />
+          We partner with owners, designers, and contractors to bring clarity to complex projects through BIM, VDC, and model-based workflows. 
+          <br />
+          Our work is grounded in practical construction knowledge and focused on delivering information that can be trusted in the field.
+          <br />
+          <br />
+          Our approach is built on three core principles:
+          <br />
+          <br />
+          Fast – Efficient workflows that accelerate coordination and decision-making
+          <br />
+          Accurate – Precise, reliable models that reduce risk and rework
+          <br />
+          Constructable – Digital deliverables aligned with real construction means and methods
+          <br/>
+          <br />
+          By prioritizing speed, accuracy, and constructability, SHD Virtual Construction helps teams plan, coordinate, and build with confidence</p>
+      </div>
+
       <div className="image-container">
         <h1>Sample</h1>
         <img src={sampleImage} width="1080px" alt="sample image" />
       </div>
 
       <div className="footer">
+        <img src={logo} />
         <a href="https://www.linkedin.com/company/shd-virtual-construction">Follow us on LinkedIn!</a>
+
       </div>
     </>
   )
